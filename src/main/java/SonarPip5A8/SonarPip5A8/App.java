1afbe03fcec11ae2b59dd18b5cb06ba6c73a0887
prasa <prasa@LAPTOP-H0PSVFQQ>
2022-12-04 16:52:15 +0530

    commit for mamatha

---
+++ src/main/java/SonarPip5A8/SonarPip5A8/App.java
@@ -0,0 +1,19 @@
+package SonarPip5A8.SonarPip5A8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );;;;
+        int a;
+        a=+15;
+        System.out.println(a);
+    }
+    public static void main(String[] args) {
+    	
+    }
+}
---
+++ src/test/java/SonarPip5A8/SonarPip5A8/AppTest.java
@@ -0,0 +1,20 @@
+package SonarPip5A8.SonarPip5A8;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
